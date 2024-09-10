349db36cb37df8ba12e53cbeb0a768f655a5f1e5
MhingFon-Odds <168728943+SoontareeRs@users.noreply.github.com>
2024-09-10 23:11:22 +0700

    add Model and DbContext

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using MyWebAPI.Models;
+using MyWebAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<UserDbContext>(option => option.UseMySql("Server=localhost;Port=3306;Database=Users;User=root;Password=password;", new MySqlServerVersion(new Version(8, 0, 29))));
 
 var app = builder.Build();
 
