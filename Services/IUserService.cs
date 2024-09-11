2d9d79b30e9cb91205886e4a2faa720f1f72718d
MhingFon-Odds <168728943+SoontareeRs@users.noreply.github.com>
2024-09-11 22:52:28 +0700

    call method delete and update

--- Services/IUserService.cs
+++ Services/IUserService.cs
@@ -7,4 +7,6 @@ public interface IUserService
     Task<List<User>> GetAllUsers();
     Task<User> CreateUser(User user);
     Task<User> GetUserById(int id);
+    Task<User> UpdateUserAsync(int id, User user);
+    Task<bool> DeleteUserAsync(int id);
 }
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -28,4 +28,13 @@ public class UserService : IUserService
         return user;
     }
 
+    public async Task<User> UpdateUserAsync(int id, User user){
+        return await _userRepository.UpdateUserAsync(id, user);
+    }
+
+    public async Task<bool> DeleteUserAsync(int id)
+    {
+        return await _userRepository.DeleteUserAsync(id);
+    }
+
 }
