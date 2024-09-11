87630df3dad9d765418a6f0bee422a2d40e58cf2
MhingFon-Odds <168728943+SoontareeRs@users.noreply.github.com>
2024-09-11 22:51:47 +0700

    add delete and update method, improve get method

--- Repositories/IUserRepository.cs
+++ Repositories/IUserRepository.cs
@@ -3,5 +3,8 @@ public interface IUserRepository
 {
     Task<List<User>> GetAllUsers();
     Task<User> CreateUser(User user);
+
     Task<User> GetUserById(int id);
+    Task<User> UpdateUserAsync(int id,User user);
+    Task<bool> DeleteUserAsync(int id);
 }
